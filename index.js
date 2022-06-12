420df02ccf96d999f93bd45dba1474ba2c3271a4
carlton <carltonmbaj0@gmail.com>
2022-06-12 10:15:48 +0300

    commit tree 87c4a0adc77d216df7e67747ef665e9029549229

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Write your code here!
+//No longer has DOM node #main
 const main = document.getElementById("main");
 document.body.removeChild(main);
 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
+
 newHeader.innerHTML = "CARL is the champion";
 document.body.appendChild(newHeader);
